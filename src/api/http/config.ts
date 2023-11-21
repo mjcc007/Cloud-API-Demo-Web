97a47136e06f0d65353c096eaba193f770291174
mjcc <834857633@qq.com>
2023-11-21 21:27:15 +0800

    feat: 配置rtmp推流地址

--- src/api/http/config.ts
+++ src/api/http/config.ts
@@ -11,7 +11,7 @@ export const CURRENT_CONFIG = {
 
   // livestreaming
   // RTMP  Note: This IP is the address of the streaming server. If you want to see livestream on web page, you need to convert the RTMP stream to WebRTC stream.
-  rtmpURL: 'rtmp://121.41.28.44/live/', // Example: 'rtmp://192.168.1.1/live/'
+  rtmpURL: 'rtmp://push.shahcl.com/live/', // 'rtmp://121.41.28.44/live/', // Example: 'rtmp://192.168.1.1/live/'
   // GB28181 Note:If you don't know what these parameters mean, you can go to Pilot2 and select the GB28181 page in the cloud platform. Where the parameters same as these parameters.
   gbServerIp: '121.41.28.44',
   gbServerPort: '15060',
